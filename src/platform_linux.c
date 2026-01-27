3913f199fe14ee4b206cd4b95550ca692c5535a7
Joris Vink <joris@sanctorum.se>
2026-01-27 22:58:56 +0100

    s/%d/%u as mtu is an uint16

--- src/platform_linux.c
+++ src/platform_linux.c
@@ -233,7 +233,7 @@ linux_tap_create(void)
 	ifr.ifr_mtu = t6->mtu;
 
 	if (ioctl(fd, SIOCSIFMTU, &ifr) == -1) {
-		fatal("failed to set mtu (%d) on tap device %s: %s",
+		fatal("failed to set mtu (%u) on tap device %s: %s",
 		    t6->mtu, t6->tapname, errno_s);
 	}
 
